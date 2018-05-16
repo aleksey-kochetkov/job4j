45e1374984628cbc2fbd0790440d2e365ac58113
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-05-16 22:58:47 +0300

    1.3. JavaDoc. Code convention[#53501]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,10 +1,24 @@
 package ru.job4j;
 
+/**
+ * Это комментарий к классу Calculate. Этот класс создан для учебного примера.
+ * @author Aleksey Kochetkov
+ */
 public class Calculate {
+
+  /**
+   * Echoes the input value three times.
+   * @param value input string
+   * @return resulting string
+   */
   public String echo(String value) {
     return String.format("%s %s %s", value, value, value);
   }
 
+  /**
+   * Main.
+   * @param args args
+   */
   public static void main(String[] args) {
     Calculate calculate = new Calculate();
     System.out.println(calculate.echo("Aah!"));
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ * This line demonstrates the description section in generated docs.
+ * @author Aleksey Kochetkov
+ * @version $Id$
+ * @since 0.1
+*/
+package ru.job4j;

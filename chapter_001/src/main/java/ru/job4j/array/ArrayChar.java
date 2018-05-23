ca3b1f0787ec292e0214fa9ceb039b7ab82a4eaa
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-05-23 13:13:27 +0300

    5.4. Слова начинается с ...[#54322] v.2

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -16,11 +16,9 @@ public class ArrayChar {
      * @return слово начинается с префикса
      */
     public boolean startsWith(String prefix) {
-        boolean result = true;
         char[] value = prefix.toCharArray();
-        if (value.length > this.data.length) {
-            result = false;
-        } else {
+        boolean result = value.length <= this.data.length;
+        if (result) {
             for (int i = 0; i < value.length; i++) {
                 if (value[i] != this.data[i]) {
                     result = false;

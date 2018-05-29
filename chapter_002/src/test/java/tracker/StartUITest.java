5f1a565cd6cdbca3205fc99540c531d140fa3d29
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-05-29 21:24:15 +0300

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя[#55276]

---
+++ chapter_002/src/test/java/tracker/StartUITest.java
@@ -0,0 +1,43 @@
+package tracker;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
+
+public class StartUITest {
+
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();     // создаём Tracker
+        Input input = new StubInput(new String[] {"0", "test name", "desc", "6"});   //создаём StubInput с последовательностью действий
+        new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
+        assertThat(tracker.getAll()[0].getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+        // создаём Tracker
+        Tracker tracker = new Tracker();
+        //Напрямую добавляем заявку
+        Item item = tracker.add(new Item("One", "Description one"));
+        //создаём StubInput с последовательностью действий
+        Input input = new StubInput(new String[] {"2", item.getId(), "test name", "desc", "6"});
+        // создаём StartUI и вызываем метод init()
+        new StartUI(input, tracker).init();
+        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+        assertThat(tracker.findById(item.getId()).getName(), is("test name"));
+    }
+
+    @Test
+    public void whenDeleteThenTrackerHasOneItem() {
+        Tracker tracker = new Tracker();
+        Item one = new Item("One", "Description one");
+        tracker.add(one);
+        Item two = new Item("Two", "Description two");
+        tracker.add(two);
+        Input input = new StubInput(new String[] {"3", one.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertEquals(1, tracker.getAll().length);
+    }
+}

dfc136bd2369096cf48595ad18aba7057f4bf73d
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-05-31 22:44:13 +0300

    5. Рефакторинг теста @Before @After[#55541]

--- chapter_002/src/test/java/pseudo/PaintTest.java
+++ chapter_002/src/test/java/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
@@ -9,17 +11,38 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void resetOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(out.toString(), is(new StringBuilder().
+                append("  +").append(System.lineSeparator()).
+                append(" + +").append(System.lineSeparator()).
+                append("+++++").toString()
+        ));
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream outSave = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(out.toString(), is(new StringBuilder().
                 append("+---+").append(System.lineSeparator()).
                 append("|   |").append(System.lineSeparator()).
                 append("+---+").toString()
         ));
-        System.setOut(outSave);
     }
 }

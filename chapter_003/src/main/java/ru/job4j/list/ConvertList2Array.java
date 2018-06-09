071bc97ff7d57730b96281b24edc0395c749f3ef
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-06-09 12:05:12 +0300

    1. Конвертация листа массивов в один лист Integer[#56712]

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,8 +1,10 @@
 package ru.job4j.list;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class ConvertList2Array {
+
     public int[][] toArray(List<Integer> list, int rows) {
         int cells = (int) Math.ceil((double) list.size() / rows);
         int[][] array = new int[rows][cells];
@@ -11,5 +13,15 @@ public class ConvertList2Array {
             array[counter / rows][counter++ % rows] = i;
         }
         return array;
+    }
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] sub : list) {
+            for (int i : sub) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -1,11 +1,14 @@
 package ru.job4j.list;
 
 import org.junit.Test;
-import java.util.Arrays;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ConvertList2ArrayTest {
+
     @Test
     public void when7ElementsThen9() {
         ConvertList2Array list = new ConvertList2Array();
@@ -18,6 +21,16 @@ public class ConvertList2ArrayTest {
                 {4, 5, 6},
                 {7, 0, 0}
         };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenConvertListOfArrays() {
+        List<int[]> input = new ArrayList<>();
+        input.add(new int[]{1, 2});
+        input.add(new int[]{3, 4, 5, 6});
+        List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> result = new ConvertList2Array().convert(input);
         assertThat(result, is(expect));
     }
 }

71a498efda50de87d8e2ec54a9bf8ef23c5dfecd
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-06-25 21:45:32 +0300

    3. Реализовать коллекцию типа Set на базе хэш-таблицы[#58358]

--- part_2_001/src/main/java/services/SimpleHashSet.java
+++ part_2_001/src/main/java/services/SimpleHashSet.java
@@ -1,14 +1,19 @@
 package services;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  * Учебно-тренировочный hash set.
  * По условиям задачи коллизии не обрабатывать!
  * Поэтому в случае коллизии данная реализация работает некорректно с
  * точки зрения здравого смысла! Реализация учебная.
  */
-public class SimpleHashSet<E> {
+public class SimpleHashSet<E> implements Iterable<E> {
     private Object[] elements = new Object[1];
     private int size = 0;
+    private int modCount;
 
     /**
      * Null не допускается, т.к. имеет внутреннее предназначение.
@@ -24,6 +29,7 @@ public class SimpleHashSet<E> {
             this.resize();
         }
         if (!this.contains(element)) {
+            this.modCount++;
             this.elements[this.hash(element)] = element;
             this.size++;
             result = true;
@@ -38,10 +44,36 @@ public class SimpleHashSet<E> {
     public boolean remove(E element) {
         boolean result = this.contains(element);
         if (result) {
+            this.modCount++;
             this.elements[this.hash(element)] = null;
             size--;
         }
         return result;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int next;
+            private int expectedModCount = SimpleHashSet.this.modCount;
+
+            @Override
+            public boolean hasNext() {
+                return this.next < SimpleHashSet.this.size;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public E next() {
+                if (this.expectedModCount != SimpleHashSet.this.modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!this.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (E) SimpleHashSet.this.elements[next++];
+            }
+        };
     }
 
     private int hash(Object element) {
--- part_2_001/src/test/java/services/SimpleHashSetTest.java
+++ part_2_001/src/test/java/services/SimpleHashSetTest.java
@@ -1,8 +1,14 @@
 package services;
 
 import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.ConcurrentModificationException;
+
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 
 public class SimpleHashSetTest {
 
@@ -53,5 +59,38 @@ public class SimpleHashSetTest {
         SimpleHashSet<Integer> set = new SimpleHashSet<>();
         set.add(0);
         assertTrue(set.add(1));
+    }
+
+    @Test
+    public void whenIterateThenLastElement() {
+        SimpleHashSet<Integer> set = new SimpleHashSet<>();
+        set.add(0);
+        set.add(1);
+        int result = 0;
+        for (Integer i : set) {
+            result = i;
+        }
+        assertEquals(1, result);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenExcessiveIterate() {
+        SimpleHashSet<Integer> set = new SimpleHashSet<>();
+        set.add(0);
+        set.add(1);
+        Iterator<Integer> it = set.iterator();
+        it.next();
+        it.next();
+        it.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenModificationWhileIterate() {
+        SimpleHashSet<Integer> set = new SimpleHashSet<>();
+        set.add(0);
+        set.add(1);
+        for (Integer i : set) {
+            set.add(2);
+        }
     }
 }

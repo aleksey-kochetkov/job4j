7d4de114ab913dce0d7da730513e1ebd483b5f34
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-09-10 23:23:37 +0300

    Валидация формы JS

--- part_2_002/src/test/java/switcher/SwitcherTest.java
+++ part_2_002/src/test/java/switcher/SwitcherTest.java
@@ -10,7 +10,7 @@ public class SwitcherTest {
         Switcher switcher = new Switcher();
         switcher.start();
         try {
-            Thread.sleep(300);
+            Thread.sleep(400);
         } catch (InterruptedException exception) {
             throw new RuntimeException(exception);
         }

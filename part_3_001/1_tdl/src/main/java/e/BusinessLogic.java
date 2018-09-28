9b2489fe751d85be91d1144e7358fd5673da4753
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-09-28 16:08:40 +0300

    Лямбды и шаблон wrapper

--- part_3_001/1_tdl/src/main/java/e/BusinessLogic.java
+++ part_3_001/1_tdl/src/main/java/e/BusinessLogic.java
@@ -5,41 +5,48 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import java.util.List;
+import java.util.function.Function;
 
 public class BusinessLogic {
+    private SessionFactory factory = new Configuration()
+                                      .configure().buildSessionFactory();
 
     Item createItem(String descript) {
-        Item result;
-        try (SessionFactory factory =
-                   new Configuration().configure().buildSessionFactory();
-             Session session = factory.openSession()) {
-            result = new Item(descript);
-            session.save(result);
-        }
-        return result;
+        Item item = new Item(descript);
+        wrapper(session -> session.save(item));
+        return item;
     }
 
     List<Item> findAllItems(String doneStr) {
-        try (SessionFactory factory =
-                   new Configuration().configure().buildSessionFactory();
-             Session session = factory.openSession()) {
+        return this.wrapper(session -> {
             String sql = Boolean.parseBoolean(doneStr) ? "from Item"
                                         : "from Item where done = false";
             Query<Item> query = session.createQuery(sql);
             return query.list();
-        }
+        });
     }
 
     Item setItemDone(String idStr, String doneStr) {
-        try (SessionFactory factory =
-                   new Configuration().configure().buildSessionFactory();
-             Session session = factory.openSession()) {
-            session.getTransaction().begin();
-            Item result = new Item();
-            session.load(result, Integer.parseInt(idStr));
-            result.setDone(Boolean.parseBoolean(doneStr));
+        Item item = new Item();
+        return this.wrapper(session -> {
+            session.load(item, Integer.parseInt(idStr));
+            item.setDone(Boolean.parseBoolean(doneStr));
+            return item;
+        });
+    }
+
+    private <T> T wrapper(Function<Session, T> function) {
+        Session session = this.factory.openSession();
+        session.getTransaction().begin();
+        try {
+            T result = function.apply(session);
             session.getTransaction().commit();
             return result;
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+            throw exception;
+        } finally {
+            session.close();
         }
     }
 }
